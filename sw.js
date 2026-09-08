932c192a9e1c1bff2dc10afefd5ba32c58108bb0
Nandalily <nankundalilian5@example.com>
2026-09-08 16:20:52 +0300

    download

--- sw.js
+++ sw.js
@@ -1,102 +1,38 @@
-const CACHE_NAME = "medidose-development-v1";
-
-const FILES = [
-    "./",
-    "./index.html",
-    "./styles.css",
-    "./app.js",
-    "./manifest.json"
+const CACHE_NAME = 'medidose-v1';
+const APP_SHELL = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png'
 ];
 
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(APP_SHELL))
+  );
+  self.skipWaiting();
+});
 
-self.addEventListener(
-    "install",
-    event => {
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.filter((k) => k !== CACHE_NAME).map((k) => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
 
-        self.skipWaiting();
-
-        event.waitUntil(
-
-            caches.open(CACHE_NAME)
-                .then(cache => {
-
-                    return cache.addAll(FILES);
-
-                })
-
-        );
-
-    }
-);
-
-
-self.addEventListener(
-    "activate",
-    event => {
-
-        event.waitUntil(
-
-            caches.keys()
-                .then(keys => {
-
-                    return Promise.all(
-
-                        keys.map(key => {
-
-                            if (
-                                key !== CACHE_NAME
-                            ) {
-
-                                return caches.delete(
-                                    key
-                                );
-
-                            }
-
-                        })
-
-                    );
-
-                })
-
-        );
-
-        self.clients.claim();
-
-    }
-);
-
-
-/*
-   Development approach:
-
-   Always try the network first.
-
-   This means changes to your HTML/CSS/JS
-   are picked up much more easily.
-*/
-
-self.addEventListener(
-    "fetch",
-    event => {
-
-        event.respondWith(
-
-            fetch(event.request)
-                .then(response => {
-
-                    return response;
-
-                })
-                .catch(() => {
-
-                    return caches.match(
-                        event.request
-                    );
-
-                })
-
-        );
-
-    }
-);
+self.addEventListener('fetch', (event) => {
+  if (event.request.method !== 'GET') return;
+  event.respondWith(
+    caches.match(event.request).then((cached) => {
+      if (cached) return cached;
+      return fetch(event.request).then((response) => {
+        const copy = response.clone();
+        caches.open(CACHE_NAME).then((cache) => cache.put(event.request, copy));
+        return response;
+      }).catch(() => cached);
+    })
+  );
+});
